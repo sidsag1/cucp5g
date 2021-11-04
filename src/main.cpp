c5e730c5dcf1997b7a3926c3f35f143c526f04c2
Siddharth Sagar Barpanda <sagarbarpanda1@gmail.com>
2021-11-04 20:14:59 +0530

    cucp5g: Add Makefile and a cpp file
    Add a copy pasted Makefile and a .cpp file for starting the project.

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+//
+// This file is a part of cucp5g open source project.
+// Copyright (c) 2021 Siddharth Sagar Barpanda.
+//
+
+#include <cstdio>
+
+int main() {
+  puts("Hello World!!");
+  return 0;
+}
